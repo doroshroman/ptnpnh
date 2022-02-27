76984d1e73d000178f486012be738d01cb02841b
Roman <doroshroman27@gmail.com>
2022-02-27 19:58:31 +0200

    added more hosts

--- open_db.py
+++ open_db.py
@@ -1,14 +1,18 @@
 import shodan
 
 
-def main():
+def write_vulns(host, vulns, file="vulns.txt"):
+    with open(file, "a+") as f:
+        f.write(f"Host: {host}: {str(vulns)}\n")
+
+def find_with_shodon():
     api_key = "D3JstZRg8nGXXZ2airRQ0MLMnjjUJJv5"
 
     api = shodan.Shodan(api_key)
     hosts = []
     while True:
         try:
-            for banner in api.search_cursor('Russia'):
+            for banner in api.search_cursor('RU'):
                 hosts.append(banner["ip_str"])
         except Exception:
             break
@@ -19,14 +23,16 @@ def main():
         host_obj = api.host(host)
         vulns = host_obj.get('vulns')
         print(f"Vulns {host}: {vulns}")
+
         # Write vulnerabilities to file
         if vulns:
             write_vulns(host, vulns)
-        
 
-def write_vulns(host, vulns, file="vulns.txt"):
-    with open(file, "a+") as f:
-        f.write(f"Host: {host}: {str(vulns)}\n")
+    
+
+def main():
+    find_with_shodon()
+
 
 if __name__ == "__main__":
     main()
