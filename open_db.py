a0effbb84fafb460df1d191684e2f702ddb654c1
Roman <doroshroman27@gmail.com>
2022-02-28 11:49:54 +0200

    updated vulns list

--- open_db.py
+++ open_db.py
@@ -1,15 +1,17 @@
-import shodan
+import shodan, settings
 
 
 def write_vulns(host, vulns, file="vulns.txt"):
     with open(file, "a+") as f:
         f.write(f"Host: {host}: {str(vulns)}\n")
 
+
 def find_with_shodon():
-    api_key = "D3JstZRg8nGXXZ2airRQ0MLMnjjUJJv5"
+    api_key = settings.SHODAN_API
 
     api = shodan.Shodan(api_key)
     hosts = []
+    
     while True:
         try:
             for banner in api.search_cursor('RU'):
@@ -29,7 +31,6 @@ def find_with_shodon():
             write_vulns(host, vulns)
 
     
-
 def main():
     find_with_shodon()
 
